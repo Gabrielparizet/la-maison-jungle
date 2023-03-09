01c1112bd1a8a1d226e738ec975c9f32943b8781
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-09 17:09:03 +0100

    (style) Add style to Cart and Banner

---
+++ la-maison-jungle/src/components/Banner.js
@@ -0,0 +1,13 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css';
+
+const Banner = () => {
+    const title = 'the jungle house';
+    return (<div className='tjh-banner'>
+        <img src={logo} alt='The jungle house' className='tjh-logo' />
+        <h1 className="tjh-title">{title.toUpperCase()}</h1>
+    </div>)
+};
+
+
+export default Banner;
