352848123c07c996ab4675de3b4ca418ea0cf039
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-10 14:04:24 +0100

    (feat) Build PlantList

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,12 +1,16 @@
 import logo from '../assets/logo.png'
 import '../styles/Banner.css';
+import Recommendation from './Recommendation'
 
 const Banner = () => {
     const title = 'the jungle house';
-    return (<div className='tjh-banner'>
-        <img src={logo} alt='The jungle house' className='tjh-logo' />
-        <h1 className="tjh-title">{title.toUpperCase()}</h1>
-    </div>)
+
+    return (
+        <div className='tjh-banner'>   
+            <img src={logo} alt='The jungle house' className='tjh-logo' />
+            <h1 className="tjh-title">{title.toUpperCase()}</h1>
+        </div>
+    )
 };
 
 
